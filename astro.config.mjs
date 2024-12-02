facc1c0dc4d09ac14e5447ea817bccbfab83565f
TarangK18 <tarangkamble18@gmail.com>
2024-12-02 17:36:28 +0530

    Add GitHub Pages deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://TarangK18.github.io',
+  base: '/Photo-Portfolio',
   integrations: [tailwind(), react()]
 });
